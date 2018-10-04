65d957e50cff211689e27cba2c632ca7edd089c4
Bruno <brunoteixeiralc@gmail.com>
2018-10-04 00:12:46 -0300

    User postgres

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,5 +1,5 @@
 import Vapor
-import FluentSQLite
+import FluentPostgreSQL
 
 final class Acronym: Codable{
   
@@ -13,7 +13,7 @@ final class Acronym: Codable{
   }
 }
 
-extension Acronym: SQLiteModel{}
+extension Acronym: PostgreSQLModel{}
 
 extension Acronym: Migration {}
 
